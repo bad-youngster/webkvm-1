e625f734c64b253b1d57fe866cff76df4c082f49
should829@gmail.com <should829@gmail.com>
2018-12-17 18:42:12 +0800

    update setting dev jenkins

---
+++ webkvm/settings-dev.py
@@ -0,0 +1,13 @@
+from webkvm.settings import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+INSTALLED_APPS +=('django_jenkins',)
+
+JENKINS_TASKS = (
+    'django_jenkins.tasks.with_coverage',
+    'django_jenkins.tasks.django_tests',
+    'django_jenkins.tasks.run_pep8',
+    'django_jenkins.tasks.run_pyflakes',
+)
